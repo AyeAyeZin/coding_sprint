097cfd5939be2c739bd86ae4f4fb21901f14f7a9
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-24 21:12:11 +0630

    Add

---
+++ python_exercises/ex11.py
@@ -0,0 +1,7 @@
+print("How old are you?", end=' ')
+age=input(38)
+print("How tall are you?", end=' ')
+height=input(120)
+print("How much do you weight?", end=' ')
+weight= input(129)
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
